7865ab12da9ebc83fc38913bc4a788d283749538
Fercasanov <121188852+Fercasanov@users.noreply.github.com>
2023-08-07 20:38:03 -0300

    cambios

--- Proyect-Ecommerce/Proyectecommerce/Proyectecommerce/settings.py
+++ Proyect-Ecommerce/Proyectecommerce/Proyectecommerce/settings.py
@@ -130,7 +130,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'  #para cuentas de gmail
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD ="" 
-EMAIL_PORT = 587
+EMAIL_PORT = 588
 EMAIL_USE_TLS = True
 
 
